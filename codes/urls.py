ca702fe37c0a50b9d1d3805752a328b67c5044d5
Sartaj Islam <sartajislam21@gmail.com>
2021-10-18 19:09:19 +0530

    CODE app

---
+++ codes/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    CodeListView,
+    CodeDetailView,
+    CodeCreateView,
+    CodeUpdateView,
+    CodeDeleteView
+)
+from . import views
+
+urlpatterns = [
+    path('', CodeListView.as_view(), name='code-home'),
+    path('codes/<int:pk>/', CodeDetailView.as_view(), name='code-detail'),
+    path('codes/new/', CodeCreateView.as_view(), name='code-create'),
+    path('codes/<int:pk>/update/', CodeUpdateView.as_view(), name='code-update'),
+    path('codes/<int:pk>/delete/', CodeDeleteView.as_view(), name='code-delete'),
+]
